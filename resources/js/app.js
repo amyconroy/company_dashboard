e2dff8c023fc5512fe695d6de74a72d66e7df0d2
Amy Conroy <53708099+amyconroy@users.noreply.github.com>
2020-08-16 13:47:44 +0100

    vue components linked

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,45 +1,31 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Using the vue router and vue axios dependencies
- * Vue-router used to route the vue.js application using different components, 
- * vue-axios sends network requests to the server. 
- */
-
-
+import Vue from "vue";
 import VueRouter from "vue-router";
+
 Vue.use(VueRouter);
 
-import VueAxios from "vue-axios";
-import axios from "axios";
-Vue.use(VueAxios, axios);
+import App from "./views/App";
+import Hello from "./views/Hello";
+import Home from "./views/Home";
 
 
-/**
- * The following block of code used to automatically register Vue components. 
- */
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home,
+        },
+        {
+            path: "/hello",
+            name: "hello",
+            component: Hello,
+        },
+    ],
+});
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component(
-    "example-component",
-    require("./components/ExampleComponent.vue")
-);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const router = new VueRouter({ mode: "history" });
-const app = new Vue(Vue.util.extend({ router })).$mount("#app");
+const app = new Vue({
+    el: "#app",
+    components: { App },
+    router,
+});
